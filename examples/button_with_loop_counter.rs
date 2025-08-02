cffbb3a56db19ab3ea648747c0fc58ec6b083989
Jacob Bush <jacobbush9@gmail.com>
2025-08-02 12:14:29 -0400

    sprites, some screen info, partial scaling

---
+++ examples/button_with_loop_counter.rs
@@ -0,0 +1,175 @@
+// https://bevy.org/examples/ui-user-interface/button/
+
+use bevy::{color::palettes::basic::*, color::palettes::css::GOLD, prelude::*};
+use bevy_svg::prelude::*;
+
+const TITLE: &str = "Buffering";
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: TITLE.to_string(),
+                resolution: (600., 600.).into(),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .add_plugins(bevy_svg::prelude::SvgPlugin)
+        .add_systems(Startup, setup)
+        .add_systems(
+            Update,
+            (button_system, loop_counter_text_update_system, change_loops_per_second, loop_counter_value_update_system),
+        )
+        .insert_resource(LoopsPerSecond(1.))
+        .run();
+}
+
+#[derive(Component, Default)]
+struct LoopCounter(f64);
+
+#[derive(Component)]
+struct LoopCounterButton;
+
+#[derive(Resource)]
+struct LoopsPerSecond(f64);
+
+const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+
+fn button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor, &mut BorderColor, &Children),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut text_query: Query<&mut Text>,
+) {
+    for (interaction, mut color, mut border_color, children) in &mut interaction_query {
+        let mut text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::Pressed => {
+                **text = "Press".to_string();
+                *color = PRESSED_BUTTON.into();
+                border_color.0 = RED.into();
+            }
+            Interaction::Hovered => {
+                **text = "Hover".to_string();
+                *color = HOVERED_BUTTON.into();
+                border_color.0 = Color::WHITE;
+            }
+            Interaction::None => {
+                **text = "Button".to_string();
+                *color = NORMAL_BUTTON.into();
+                border_color.0 = Color::BLACK;
+            }
+        }
+    }
+}
+
+fn setup(mut commands: Commands, assets: Res<AssetServer>) {
+    // ui camera
+    commands.spawn(Camera2d);
+    commands.spawn(loop_counter_button(&assets));
+    commands.spawn(counter(&assets));
+
+    let svg = assets.load("icon.svg");
+    commands.spawn((
+        Svg2d(svg),
+        // Origin::TopRight,
+        // Transform { translation: Vec3::new(100.0, 0.0, 0.0), scale: Vec3::new(5., 5., 1.0), ..Default::default() },
+    ));
+}
+
+fn counter(asset_server: &AssetServer) -> impl Bundle {
+    (
+        // Create a Text with multiple child spans.
+        Text::new("L∞ps: "),
+        TextFont {
+            // This font is loaded and will be used instead of the default font.
+            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+            font_size: 42.0,
+            ..default()
+        },
+        children![(
+            LoopCounter::default(),
+            TextSpan::default(),
+            TextFont { font: asset_server.load("fonts/FiraMono-Medium.ttf"), font_size: 33.0, ..Default::default() },
+            TextColor(GOLD.into()),
+        )],
+    )
+}
+
+fn loop_counter_value_update_system(
+    time: Res<Time>,
+    loops_per_second: Res<LoopsPerSecond>,
+    mut loop_counters: Query<&mut LoopCounter>,
+    loop_counter_button_interactions: Query<&Interaction, With<LoopCounterButton>>,
+) {
+    // Rather than doing this, we should probably make loop_counter.0 a floating point number
+    // Then increase it by the timedelta on each frame times the current value per time
+    // (We can truncate to an integer if we care about the display.)
+    for mut loop_counter in &mut loop_counters {
+        for interaction in loop_counter_button_interactions {
+            if let Interaction::Pressed = interaction {
+                loop_counter.0 += time.delta_secs_f64() * loops_per_second.0;
+            }
+        }
+    }
+}
+fn change_loops_per_second(mut loops_per_second: ResMut<LoopsPerSecond>, keys: Res<ButtonInput<KeyCode>>) {
+    if keys.just_pressed(KeyCode::ArrowUp) {
+        // Increase updates-per-second
+        let original = loops_per_second.0;
+        let target = original * 2.;
+        loops_per_second.0 = target;
+        println!("Loops per second changed from {original:.1} to {target:.1}");
+    } else if keys.just_pressed(KeyCode::ArrowDown) {
+        // Decrease updates-per-second
+        let original = loops_per_second.0;
+        let target = original / 2.;
+        loops_per_second.0 = target;
+        println!("Loops per second changed from {original:.1} to {target:.1}");
+    }
+}
+
+fn loop_counter_text_update_system(mut query: Query<(&mut TextSpan, &LoopCounter)>) {
+    for (mut span, &LoopCounter(loops)) in &mut query {
+        **span = format!("{}", loops as u64);
+    }
+}
+
+fn loop_counter_button(asset_server: &AssetServer) -> impl Bundle + use<> {
+    (
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+        children![(
+            LoopCounterButton,
+            Button,
+            Node {
+                width: Val::Px(150.0),
+                height: Val::Px(65.0),
+                border: UiRect::all(Val::Px(5.0)),
+                // horizontally center child text
+                justify_content: JustifyContent::Center,
+                // vertically center child text
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            BorderColor(Color::BLACK),
+            BorderRadius::MAX,
+            BackgroundColor(NORMAL_BUTTON),
+            children![(
+                Text::new("Button"),
+                TextFont { font: asset_server.load("fonts/FiraSans-Bold.ttf"), font_size: 33.0, ..default() },
+                TextColor(Color::srgb(0.9, 0.9, 0.9)),
+                TextShadow::default(),
+            )]
+        )],
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,157 +1,90 @@
-// https://bevy.org/examples/ui-user-interface/button/
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+use bevy::window::WindowResized;
+use bevy_svg::prelude::*;
+use clap::Parser;
 
-use bevy::{color::palettes::basic::*, color::palettes::css::GOLD, prelude::*};
+// A resource to store the previous window size
+// Assumes a single window
+#[derive(Resource, Default)]
+struct PreviousWindowSize {
+    width: f32,
+    height: f32,
+}
+
+fn on_resize_system(
+    mut prev_size: ResMut<PreviousWindowSize>,
+    mut sprites: Query<(&mut Transform, &Svg2d)>,
+    mut resize_reader: EventReader<WindowResized>,
+    svgs: Res<Assets<Svg>>,
+) {
+    for e in resize_reader.read() {
+        // When resolution is being changed
+        println!("previous: {}, {}", prev_size.width, prev_size.height);
+        println!("window: {}, {}", e.width, e.height);
+        for (sprite_transform, svg2d) in &mut sprites {
+            println!("{}", sprite_transform.scale);
+            if let Some(svg) = svgs.get(&svg2d.0) {
+                println!("{}", svg.size)
+            }
+        }
+        prev_size.width = e.width;
+        prev_size.height = e.height;
+    }
+}
+
+const DEFAULT_INTERNET: &str = "icons/000000/transparent/1x1/delapouite/wireframe-globe.svg";
+// icons/000000/transparent/1x1/delapouite/cloud-upload.svg
+// icons/000000/transparent/1x1/delapouite/cloud-download.svg
+const DEFAULT_WIFI: &str = "icons/000000/transparent/1x1/delapouite/wifi-router.svg";
+const DEFAULT_COMPUTER: &str = "icons/000000/transparent/1x1/skoll/pc.svg";
+const INITIAL_WIDTH: f32 = 2000.;
+const INITIAL_HEIGHT: f32 = 1000.;
+
+/// Configure the application at runtime
+#[derive(Parser, Debug, Resource)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// internet sprite svg path relative to assets folder
+    #[arg(short, long, default_value_t = String::from(DEFAULT_INTERNET))]
+    internet: String,
+
+    /// computer sprite svg path relative to assets folder
+    #[arg(short, long, default_value_t = String::from(DEFAULT_WIFI))]
+    wifi: String,
+
+    /// computer sprite svg path relative to assets folder
+    #[arg(short, long, default_value_t = String::from(DEFAULT_COMPUTER))]
+    computer: String,
+}
+
+fn setup(mut commands: Commands, assets: Res<AssetServer>, args: Res<Args>) {
+    commands.spawn(Camera2d::default());
+    commands.spawn((
+        Svg2d(assets.load(&args.internet)),
+        Transform { translation: Vec3::new(-INITIAL_WIDTH / 2., INITIAL_HEIGHT / 2., 0.), ..Default::default() },
+        Origin::TopLeft,
+    ));
+    commands.spawn((Svg2d(assets.load(&args.wifi)), Origin::Center));
+    commands.spawn((Svg2d(assets.load(&args.computer)), Origin::Center));
+}
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .insert_resource(Args::parse())
+        .insert_resource(PreviousWindowSize::default())
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Buffering".to_string(),
+                    resolution: (INITIAL_WIDTH, INITIAL_HEIGHT).into(),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            }),
+            bevy_svg::prelude::SvgPlugin,
+        ))
         .add_systems(Startup, setup)
-        .add_systems(
-            Update,
-            (button_system, loop_counter_text_update_system, change_loops_per_second, loop_counter_value_update_system),
-        )
-        .insert_resource(LoopsPerSecond(1.))
+        .add_systems(Update, on_resize_system)
         .run();
-}
-
-#[derive(Component, Default)]
-struct LoopCounter(f64);
-
-#[derive(Component)]
-struct LoopCounterButton;
-
-#[derive(Resource)]
-struct LoopsPerSecond(f64);
-
-const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
-const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
-const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
-
-fn button_system(
-    mut interaction_query: Query<
-        (&Interaction, &mut BackgroundColor, &mut BorderColor, &Children),
-        (Changed<Interaction>, With<Button>),
-    >,
-    mut text_query: Query<&mut Text>,
-) {
-    for (interaction, mut color, mut border_color, children) in &mut interaction_query {
-        let mut text = text_query.get_mut(children[0]).unwrap();
-        match *interaction {
-            Interaction::Pressed => {
-                **text = "Press".to_string();
-                *color = PRESSED_BUTTON.into();
-                border_color.0 = RED.into();
-            }
-            Interaction::Hovered => {
-                **text = "Hover".to_string();
-                *color = HOVERED_BUTTON.into();
-                border_color.0 = Color::WHITE;
-            }
-            Interaction::None => {
-                **text = "Button".to_string();
-                *color = NORMAL_BUTTON.into();
-                border_color.0 = Color::BLACK;
-            }
-        }
-    }
-}
-
-fn setup(mut commands: Commands, assets: Res<AssetServer>) {
-    // ui camera
-    commands.spawn(Camera2d);
-    commands.spawn(loop_counter_button(&assets));
-    commands.spawn(counter(&assets));
-}
-
-fn counter(asset_server: &AssetServer) -> impl Bundle {
-    (
-        // Create a Text with multiple child spans.
-        Text::new("L∞ps: "),
-        TextFont {
-            // This font is loaded and will be used instead of the default font.
-            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-            font_size: 42.0,
-            ..default()
-        },
-        children![(
-            LoopCounter::default(),
-            TextSpan::default(),
-            TextFont { font: asset_server.load("fonts/FiraMono-Medium.ttf"), font_size: 33.0, ..Default::default() },
-            TextColor(GOLD.into()),
-        )],
-    )
-}
-
-fn loop_counter_value_update_system(
-    time: Res<Time>,
-    loops_per_second: Res<LoopsPerSecond>,
-    mut loop_counters: Query<&mut LoopCounter>,
-    loop_counter_button_interactions: Query<&Interaction, With<LoopCounterButton>>,
-) {
-    // Rather than doing this, we should probably make loop_counter.0 a floating point number
-    // Then increase it by the timedelta on each frame times the current value per time
-    // (We can truncate to an integer if we care about the display.)
-    for mut loop_counter in &mut loop_counters {
-        for interaction in loop_counter_button_interactions {
-            if let Interaction::Pressed = interaction {
-                loop_counter.0 += time.delta_secs_f64() * loops_per_second.0;
-            }
-        }
-    }
-}
-fn change_loops_per_second(mut loops_per_second: ResMut<LoopsPerSecond>, keys: Res<ButtonInput<KeyCode>>) {
-    if keys.just_pressed(KeyCode::ArrowUp) {
-        // Increase updates-per-second
-        let original = loops_per_second.0;
-        let target = original * 2.;
-        loops_per_second.0 = target;
-        println!("Loops per second changed from {original:.1} to {target:.1}");
-    } else if keys.just_pressed(KeyCode::ArrowDown) {
-        // Decrease updates-per-second
-        let original = loops_per_second.0;
-        let target = original / 2.;
-        loops_per_second.0 = target;
-        println!("Loops per second changed from {original:.1} to {target:.1}");
-    }
-}
-
-fn loop_counter_text_update_system(mut query: Query<(&mut TextSpan, &LoopCounter)>) {
-    for (mut span, &LoopCounter(loops)) in &mut query {
-        **span = format!("{}", loops as u64);
-    }
-}
-
-fn loop_counter_button(asset_server: &AssetServer) -> impl Bundle + use<> {
-    (
-        Node {
-            width: Val::Percent(100.0),
-            height: Val::Percent(100.0),
-            align_items: AlignItems::Center,
-            justify_content: JustifyContent::Center,
-            ..default()
-        },
-        children![(
-            LoopCounterButton,
-            Button,
-            Node {
-                width: Val::Px(150.0),
-                height: Val::Px(65.0),
-                border: UiRect::all(Val::Px(5.0)),
-                // horizontally center child text
-                justify_content: JustifyContent::Center,
-                // vertically center child text
-                align_items: AlignItems::Center,
-                ..default()
-            },
-            BorderColor(Color::BLACK),
-            BorderRadius::MAX,
-            BackgroundColor(NORMAL_BUTTON),
-            children![(
-                Text::new("Button"),
-                TextFont { font: asset_server.load("fonts/FiraSans-Bold.ttf"), font_size: 33.0, ..default() },
-                TextColor(Color::srgb(0.9, 0.9, 0.9)),
-                TextShadow::default(),
-            )]
-        )],
-    )
 }
